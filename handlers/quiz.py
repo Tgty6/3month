6ec1fe94fb518b22f87694254b8ad9ceebfe2112
Bayastan <baastana7@gmail.com>
2025-01-31 02:03:17 +0600

    Сделано 2 домашее задание

--- handlers/quiz.py
+++ handlers/quiz.py
@@ -1,6 +1,7 @@
 from aiogram import types, Dispatcher
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import bot
+import os
 
 
 async def quiz_1(message: types.Message):
@@ -12,11 +13,24 @@ async def quiz_1(message: types.Message):
     answer = ['RM', 'Barcelona', 'Оба']
 
     await bot.send_poll(
+        chat_id=message.chat.id,
+        question=question,
+        options=answer,
+        is_anonymous=False,
+        type='quiz',
+        correct_option_id=2,
+        explanation='Жаль...',
+        open_period=60,
+        reply_markup=keyboard
     )
 
 async def quiz_2(call: types.CallbackQuery):
     question = 'Dota2 or CS.GO'
     answer = ['Dota2', 'CS.GO']
+
+    keyboard = InlineKeyboardMarkup(resize_keyboard=True)
+    button = InlineKeyboardButton('Далее', callback_data='button2')
+    keyboard.add(button)
 
     await bot.send_poll(
         chat_id=call.from_user.id,
@@ -26,8 +40,38 @@ async def quiz_2(call: types.CallbackQuery):
         type='quiz',
         correct_option_id=1,
         explanation='Эх ты...',
+        open_period=60,
+        reply_markup=keyboard
+    )
+
+
+async def quiz_3(call: types.CallbackQuery):
+    photo_path = os.path.join('media', 'img.png')
+
+    photo = open(photo_path, 'rb')
+
+    if photo:
+        await bot.send_photo(chat_id=call.from_user.id, photo=photo)
+
+        question = 'War Thunder or World of Tanks'
+        answer = ['War Thunder', 'World of Tanks']
+
+
+        await bot.send_poll(
+            chat_id=call.from_user.id,
+            question=question,
+            options=answer,
+            is_anonymous=False,
+            type='quiz',
+            correct_option_id=0,
+            explanation='Жаль...',
+            open_period=60,
+            )
+    else:
+        await call.message.answer("Ошибка: фото не найдено.")
 
 
 def register_handlers(dp: Dispatcher):
     dp.register_message_handler(quiz_1, commands=['quiz'])
     dp.register_callback_query_handler(quiz_2, text='button1')
+    dp.register_callback_query_handler(quiz_3, text='button2')
